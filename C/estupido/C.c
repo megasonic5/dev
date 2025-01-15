b079e07c1b26cb207471d4ae9b0624e3982caa38
megasonic5 <168662773+megasonic5@users.noreply.github.com>
2025-01-15 20:37:33 +0100

    leer cadena (C/estupido)

--- C/estupido/C.c
+++ C/estupido/C.c
@@ -4,19 +4,18 @@
 typedef int numero;
 typedef char texto;
 typedef enum enumeracion;
-int imprimir(const char *cadena,...) {
+void imprimir(const char *cadena,...) {
     va_list a;
     va_start(a, cadena);
     vprintf(cadena, a);
     va_end(a);
 }
-#include <stdio.h>
-#include <stdarg.h>
 
-void leer_datos(const char *formato, ...) {
-    va_list args;
-        va_start(args, formato);
-            vscanf(formato, args);
-                va_end(args);
-                }
+void leer_cadena(const char *cadena,...) {
+    va_list a;
+    va_start(a, cadena);
+    vscanf(cadena, a);
+    va_end(a);
+}
+
 #define NULO ((void*)0)
